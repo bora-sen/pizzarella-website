d68b76ef363a11eb18149745c695f051b1c59b6d
bora-sen <bahattinborasen@gmail.com>
2023-07-24 12:00:54 +0300

    +updated pizza pictures

--- src/Pages/Home/Sections/Hero.jsx
+++ src/Pages/Home/Sections/Hero.jsx
@@ -2,14 +2,16 @@ import React from "react"
 
 function Hero() {
   return (
-    <section className="bg-[url(https://via.placeholder.com/1280x720)] bg-cover bg-center w-full min-h-[50rem] py-4 flex items-center">
-      <div className="max-w-[45rem] ml-[5rem]">
+    <section className="text-white bg-[url(/assets/images/pizza-hero.jpg)] bg-cover bg-center w-full min-h-[50rem] py-4 flex items-center">
+      <div className="max-w-[42rem] ml-[5rem] backdrop-blur-sm backdrop-brightness-50 p-4">
         <h1 className="text-8xl font-bold">Get Your Pizzas!</h1>
         <p className="text-2xl mb-6">
           Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eveniet, ut sit hic dolore ullam voluptate adipisci vero esse maiores, eligendi doloribus
           omnis sunt fuga cum!
         </p>
-        <a className="bg-black px-2 py-4 text-white rounded-2xl font-bold ">Get Your Pizza</a>
+        <a href="#" className="my-4 bg-white px-2 py-4 text-black rounded-2xl font-bold ">
+          Get Your Pizza
+        </a>
       </div>
     </section>
   )
